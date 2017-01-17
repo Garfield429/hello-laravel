957e3ee339138b0dfbd6eedf2cf86a10585b53ed
Garfield429 <1065889785@qq.com>
2017-01-17 15:45:15 +0800

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,11 +6,38 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class UsersController extends Controller
 {
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+           'name' => $request->name,
+           'email' => $request->email,
+           'password' => $request->password,
+       ]);
+
+       session()->flash('success', '注册成功，欢迎来到laravel');
+
+       return redirect()->route('users.show', [$user]);
+    }
+
     public function create()
     {
-        return view('users.create');
+          return view('users.create');
     }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('users.show', compact('user'));
+    }
+
 }
